892c4dcc0855b2c9b7f36d004e885977d409d623
Alex Levinson <gen0cide.threats@gmail.com>
2013-10-26 10:30:07 -0700

    test

--- linux/ubuntu_functions.sh
+++ linux/ubuntu_functions.sh
@@ -1,40 +1,39 @@
 ubuntu_packages ()
 {
-  $SUDO="sudo"
-  $SUDO apt-get -y update
-  $SUDO apt-get -y upgrade
-  $SUDO apt-get -y install build-essential    \
-                           openssl            \
-                           libreadline6       \
-                           libreadline6-dev   \
-                           libncurses-dev     \
-                           libffi-dev         \
-                           libgdbm-dev        \
-                           zlib1g             \
-                           zlib1g-dev         \
-                           libssl-dev         \
-                           libyaml-dev        \
-                           libsqlite3-0       \
-                           libsqlite3-dev     \
-                           sqlite3            \
-                           libxml2-dev        \
-                           libxslt1-dev       \
-                           autoconf           \
-                           libc6-dev          \
-                           automake           \
-                           libtool            \
-                           git-core           \
-                           curl               \
-                           libmysqlclient-dev \
-                           unzip              \
-                           zip                \
-                           htop               \
-                           finger             \
-                           multitail          \
-                           postgresql-client  \
-                           libpq5             \
-                           libpq-dev          \
-                           lsof               
+  sudo apt-get -y update
+  sudo apt-get -y upgrade
+  sudo apt-get -y install build-essential    \
+                          openssl            \
+                          libreadline6       \
+                          libreadline6-dev   \
+                          libncurses-dev     \
+                          libffi-dev         \
+                          libgdbm-dev        \
+                          zlib1g             \
+                          zlib1g-dev         \
+                          libssl-dev         \
+                          libyaml-dev        \
+                          libsqlite3-0       \
+                          libsqlite3-dev     \
+                          sqlite3            \
+                          libxml2-dev        \
+                          libxslt1-dev       \
+                          autoconf           \
+                          libc6-dev          \
+                          automake           \
+                          libtool            \
+                          git-core           \
+                          curl               \
+                          libmysqlclient-dev \
+                          unzip              \
+                          zip                \
+                          htop               \
+                          finger             \
+                          multitail          \
+                          postgresql-client  \
+                          libpq5             \
+                          libpq-dev          \
+                          lsof               
   echo "[*] Finished installing Ubuntu packages!"
 }
 
@@ -44,3 +43,10 @@ ubuntu_rvm ()
   echo "[*] Finished installing RVM!"
   which rvm 
 }
+
+ubuntu_nano ()
+{
+  curl -sL https://raw.github.com/gen0cide-/pungi/master/nanorc | sudo tee /etc/nanorc > /dev/null
+  echo "[*] Finished installing nano config!"
+}
+
