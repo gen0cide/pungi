26dd1cf00452769178f2cce53ceaba3ba477c7c7
gen0cide- <gen0cide.threats@gmail.com>
2014-09-30 22:09:34 -0700

    2.1.3

--- linux/ubuntu_functions.sh
+++ linux/ubuntu_functions.sh
@@ -1,4 +1,4 @@
-PUNGI_RUBY_VERSION="2.1.2"
+PUNGI_RUBY_VERSION="2.1.3"
 PUNGI_DEFAULT_GEMSET="base"
 # -----------------------------------------------------------------------------
 ubuntu_log_info ()
