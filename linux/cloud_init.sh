a2ea5b2cab48403aad2808d26a8108bdfb566e48
gen0cide- <gen0cide.threats@gmail.com>
2015-02-11 15:25:27 -0800

    add this file
    ok here we go

---
+++ linux/cloud_init.sh
@@ -0,0 +1,33 @@
+# ------------------------------------------------------------------------------------------------------------
+PUNGI_RUBY_VERSION="2.2.0"
+PUNGI_DEFAULT_GEMSET="base"
+# ------------------------------------------------------------------------------------------------------------
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -L https://get.rvm.io | sudo bash -s stable
+source /etc/profile.d/rvm.sh
+sudo usermod -a -G rvm $(whoami)
+rvm autolibs enable
+rvm install $PUNGI_RUBY_VERSION
+rvm use $PUNGI_RUBY_VERSION@$PUNGI_DEFAULT_GEMSET --default --create
+# ------------------------------------------------------------------------------------------------------------
+curl -sL https://raw.github.com/gen0cide-/pungi/master/nanorc | sudo tee /etc/nanorc > /dev/null
+# ------------------------------------------------------------------------------------------------------------
+echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main 9.4" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# ------------------------------------------------------------------------------------------------------------
+add-apt-repository -y ppa:chris-lea/redis-server
+add-apt-repository -y ppa:nginx/stable
+add-apt-repository -y ppa:chris-lea/node.js
+# ------------------------------------------------------------------------------------------------------------
+apt-get update
+# ------------------------------------------------------------------------------------------------------------
+apt-get -y install nodejs postgresql-9.4 postgresql-contrib-9.4 postgresql-client-9.4 nginx uuid redis-server
+# ------------------------------------------------------------------------------------------------------------
+service postgresql stop
+service nginx stop
+service redis-server stop
+# ------------------------------------------------------------------------------------------------------------
+/usr/sbin/update-rc.d postgresql disable
+/usr/sbin/update-rc.d nginx disable
+/usr/sbin/update-rc.d redis-server disable
+# ------------------------------------------------------------------------------------------------------------
