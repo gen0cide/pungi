6a1f3b342eca9e61d403ab5bc4be150e0d7eb920
gen0cide- <gen0cide.threats@gmail.com>
2016-06-15 23:11:23 -0700

    updated PS1

--- linux/ubuntu_ps1_profile.sh
+++ linux/ubuntu_ps1_profile.sh
@@ -8,14 +8,22 @@ function parse_git_branch () {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 # ------------------------------------------------------------------------------
-RED="\[\033[0;31m\]"
-YELLOW="\[\033[0;33m\]"
-GREEN="\[\033[0;32m\]"
-GRAY="\[\033[1;30m\]"
-LIGHT_GRAY="\[\033[0;37m\]"
-CYAN="\[\033[0;36m\]"
-LIGHT_CYAN="\[\033[1;36m\]"
 NO_COLOUR="\[\033[0m\]"
+RED="\[\033[00;31m\]"
+GREEN="\[\033[00;32m\]"
+YELLOW="\[\033[00;33m\]"
+BLUE="\[\033[00;34m\]"
+MAGENTA="\[\033[00;35m\]"
+PURPLE="\[\033[00;35m\]"
+CYAN="\[\033[00;36m\]"
+LIGHTGRAY="\[\033[00;37m\]"
+LRED="\[\033[01;31m\]"
+LGREEN="\[\033[01;32m\]"
+LYELLOW="\[\033[01;33m\]"
+LBLUE="\[\033[01;34m\]"
+LMAGENTA="\[\033[01;35m\]"
+LPURPLE="\[\033[01;35m\]"
+LCYAN="\[\033[01;36m\]"
+WHITE="\[\033[01;37m\]"
 # ------------------------------------------------------------------------------
-PS1="$YELLOW\h$NO_COLOUR/$GREEN\u $NO_COLOUR@\W <$CYAN\$(rvm-prompt v g)$NO_COLOUR>$YELLOW\$(parse_git_branch)$NO_COLOUR \$ "
-# ------------------------------------------------------------------------------
+PS1="$LRED\t $LYELLOW\h$NO_COLOUR/$GREEN\u $WHITE@\W$NO_COLOUR <$LCYAN\$(rvm-prompt v g)$NO_COLOUR>$YELLOW\$(parse_git_branch)$NO_COLOUR \\$ "
