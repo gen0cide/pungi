77b756ec366f91f47d795f3e39b8e0b011198490
Alex Levinson <gen0cide.threats@gmail.com>
2022-04-16 18:30:12 -0400

    Create base_linux.sh

---
+++ linux/base_linux.sh
@@ -0,0 +1,83 @@
+# -----------------------------------------------------------------------------
+ubuntu_log_info ()
+{
+  echo ""
+  echo "######################################################################"
+  echo "#"
+  echo "# <$(date)> $1"
+  echo "#"
+  echo "######################################################################"
+  echo ""
+}
+# -----------------------------------------------------------------------------
+ubuntu_packages ()
+{
+  sudo apt-get -y update
+  sudo apt-get -y upgrade
+  sudo apt-get -y install build-essential    \
+                          openssl            \
+                          libncurses-dev     \
+                          libffi-dev         \
+                          libgdbm-dev        \
+                          zlib1g             \
+                          zlib1g-dev         \
+                          libssl-dev         \
+                          libyaml-dev        \
+                          libsqlite3-0       \
+                          libsqlite3-dev     \
+                          sqlite3            \
+                          libxml2-dev        \
+                          libxslt1-dev       \
+                          autoconf           \
+                          libc6-dev          \
+                          automake           \
+                          libtool            \
+                          git-core           \
+                          curl               \
+                          libmysqlclient-dev \
+                          unzip              \
+                          zip                \
+                          htop               \
+                          finger             \
+                          multitail          \
+                          postgresql-client  \
+                          libpq5             \
+                          libpq-dev          \
+                          lsof               \
+                          libjemalloc-dev    \
+                          libgmp-dev         \
+                          graphviz           \
+                          traceroute         \
+                          dnsutils           \
+                          libmagic-dev       \
+                          bison
+  ubuntu_log_info "Finished installing Ubuntu packages!"
+}
+# -----------------------------------------------------------------------------
+ubuntu_nano ()
+{
+  curl -sL https://raw.github.com/gen0cide-/pungi/master/nanorc | sudo tee /etc/nanorc > /dev/null
+  ubuntu_log_info "Finished installing nano config!"
+}
+# -----------------------------------------------------------------------------
+ubuntu_ps1 ()
+{
+  curl -sL https://raw.github.com/gen0cide-/pungi/master/linux/ubuntu_base_ps1_profile.sh | sudo tee /etc/profile.d/Z1_PS1.sh > /dev/null
+  sudo chmod +x /etc/profile.d/Z1_PS1.sh
+  echo "" >> ~/.bashrc
+  echo "source /etc/profile.d/Z1_PS1.sh" >> ~/.bashrc
+  echo "" | sudo tee /etc/skel/.bashrc > /dev/null
+  echo "source /etc/profile.d/Z1_PS1.sh" | sudo tee /etc/skel/.bashrc > /dev/null
+  source /etc/profile.d/Z1_PS1.sh
+  ubuntu_log_info "Finished setting up global PS1 variable!"
+}
+# -----------------------------------------------------------------------------
+ubuntu_packages
+ubuntu_nano
+ubuntu_ps1
+# -----------------------------------------------------------------------------
+ubuntu_log_info "IMPORTANT! You must source /etc/profile for this to function successfully!"
+# -----------------------------------------------------------------------------
+source /etc/profile
+touch /root/.hushlogin
+# -----------------------------------------------------------------------------
