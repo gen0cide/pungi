7af07c788d7770fb1eb313bb30f34b02f565be7f
Alex Levinson <gen0cide.threats@gmail.com>
2022-04-16 18:46:34 -0400

    Update ubuntu_base_ps1_profile.sh

--- linux/ubuntu_base_ps1_profile.sh
+++ linux/ubuntu_base_ps1_profile.sh
@@ -8,6 +8,10 @@ function parse_git_branch () {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1) /'
 }
 # ------------------------------------------------------------------------------
+function get_assigned_ip () {
+  ip -o -4 addr list | sed '/: lo/d' | awk '{print $4}' | cut -d/ -f1
+}
+# ------------------------------------------------------------------------------
 NO_COLOUR="\[\033[0m\]"
 RED="\[\033[00;31m\]"
 GREEN="\[\033[00;32m\]"
@@ -26,5 +30,5 @@ LPURPLE="\[\033[01;35m\]"
 LCYAN="\[\033[01;36m\]"
 WHITE="\[\033[01;37m\]"
 # ------------------------------------------------------------------------------
-PS1="$LRED\t $LYELLOW\h$NO_COLOUR/$GREEN\u $WHITE@\W$NO_COLOUR $YELLOW\$(parse_git_branch)$NO_COLOUR\\$ "
+PS1="$LRED\t $LYELLOW\h$NO_COLOUR/$GREEN\u $WHITE@\W$NO_COLOUR <$LCYAN\$(get_assigned_ip)$NO_COLOUR>$YELLOW\$(parse_git_branch)$NO_COLOUR \\$ "
 # ------------------------------------------------------------------------------
